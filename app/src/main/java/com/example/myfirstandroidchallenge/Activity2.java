ca2ef753d1d6228cb9b0b7d924de9abb87e28478
Atim Winniefred <winniefredatimplexa@gmail.com>
2019-07-16 16:15:30 +0300

    add challenge

---
+++ app/src/main/java/com/example/myfirstandroidchallenge/Activity2.java
@@ -0,0 +1,16 @@
+package com.example.myfirstandroidchallenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+
+    }
+}
---
+++ app/src/main/java/com/example/myfirstandroidchallenge/Activity3.java
@@ -0,0 +1,17 @@
+package com.example.myfirstandroidchallenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Activity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_3);
+
+
+    }
+}
